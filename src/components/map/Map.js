122a618ebaf782fb3e0762e992e92a3bd738c23d
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2023-02-09 16:50:21 +0100

    feat: final

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./map.css";
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
